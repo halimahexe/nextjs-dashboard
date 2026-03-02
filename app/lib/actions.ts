be461da403887cd339d99eae2005d1b614ba5e6a
Halimah Manan <127022084+halimahexe@users.noreply.github.com>
2026-03-02 13:42:35 +0000

    completed chapter 12

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,66 +1,99 @@
-'use server';
+"use server";
 
-import { z } from 'zod';
-import { revalidatePath } from 'next/cache';
-import postgres from 'postgres';
-import { redirect } from 'next/navigation';
+import { z } from "zod";
+import { revalidatePath } from "next/cache";
+import postgres from "postgres";
+import { redirect } from "next/navigation";
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
-	id: z.string(),
-	customerId: z.string(),
-	amount: z.coerce.number(),
-	status: z.enum(['pending', 'paid']),
-	date: z.string(),
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export const createInvoice = async (formData: FormData) => {
-	const { customerId, amount, status } = CreateInvoice.parse({
-		customerId: formData.get('customerId'),
-		amount: formData.get('amount'),
-		status: formData.get('status'),
-	});
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
 
-	const amountInCents = amount * 100;
-	const date = new Date().toISOString().split('T')[0];
+  if (!customerId || !amount || !status) {
+    throw new Error("Missing information");
+  }
 
-	await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
 
-	revalidatePath('/dashboard/invoices');
-	redirect('/dashboard/invoices');
+  try {
+    await sql`
+	        INSERT INTO invoices (customer_id, amount, status, date)
+	        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+	    `;
+  } catch (error) {
+    console.error(error);
+    return {
+      message: "Database Error: Failed to create invoice",
+    };
+  }
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 };
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export const updateInvoice = async (id: string, formData: FormData) => {
-	const { customerId, amount, status } = UpdateInvoice.parse({
-		customerId: formData.get('customerId'),
-		amount: formData.get('amount'),
-		status: formData.get('status'),
-	});
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
 
-	const amountInCents = amount * 100;
+  if (!customerId || !amount || !status) {
+    throw new Error("Missing information");
+  }
 
-	await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-    `;
+  const amountInCents = amount * 100;
 
-	revalidatePath('/dashboard/invoices');
-	redirect('/dashboard/invoices');
+  try {
+    await sql`
+	        UPDATE invoices
+	        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+	        WHERE id = ${id}
+	    `;
+  } catch (error) {
+    console.error(error);
+    return {
+      message: "Database error: Failed to update invoice",
+    };
+  }
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 };
 
 export const deleteInvoice = async (id: string) => {
-	await sql`
-        DELETE FROM invoices WHERE id = ${id}
-    `;
+  if (!id) {
+    throw new Error("Missing information");
+  }
 
-	revalidatePath('/dashboard/invoices');
+  try {
+    await sql`
+	        DELETE FROM invoices WHERE id = ${id}
+	    `;
+  } catch (error) {
+    console.error(error);
+    return {
+      message: "Database error: Failed to delete invoice",
+    };
+  }
+
+  revalidatePath("/dashboard/invoices");
 };
--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -1,8 +1,8 @@
-import Image from 'next/image';
-import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
-import InvoiceStatus from '@/app/ui/invoices/status';
-import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
-import { fetchFilteredInvoices } from '@/app/lib/data';
+import Image from "next/image";
+import { UpdateInvoice, DeleteInvoice } from "@/app/ui/invoices/buttons";
+import InvoiceStatus from "@/app/ui/invoices/status";
+import { formatDateToLocal, formatCurrency } from "@/app/lib/utils";
+import { fetchFilteredInvoices } from "@/app/lib/data";
 
 export default async function InvoicesTable({
   query,
@@ -14,7 +14,7 @@ export default async function InvoicesTable({
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
   return (
-    <div className="mt-6 flow-root">
+    <div className="mt-6 flow-root overflow-x-auto">
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
